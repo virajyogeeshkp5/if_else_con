85887955fdd982e513db9e3dc5ded9493337a39a
YOGEESH K P <yogeesh793@gmail.com>
2024-10-26 11:23:42 +0530

    Add files via upload

---
+++ Conditional_control_stracture_if_else.py
@@ -0,0 +1,21 @@
+"""
+1.Create a python program to accept name and age and desplay weather he/she senear citizan or not
+"""
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+if age>=60:
+    print(name,"you are seniar citizan")
+    print("Thank you")
+else:
+    print(name,"you are not a seniar citizan")
+print("you are successfully learn if else conditional statement")
+
+"""
+2. create a python program to find the eligibility for degree course in city univercity
+"""
+eli=input("Do you have pass certificate sec level?(yes/no): ")
+if eli=="yes":
+    print("you are eligable for degree course" )
+else:
+    print("get the pass certificate for sec level")
+    
